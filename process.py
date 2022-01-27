0a071f7a7a978c1cfe83011824713c467d2cf971
McKenzie Allen <mckenzie.allen144@gmail.com>
2022-01-26 19:53:35 -0700

    all processes answered

--- process.py
+++ process.py
@@ -1,12 +1,13 @@
-log_file = open("um-server-01.txt") #this Selects the file 
+log_file = open("um-server-01.txt") # this is connecting the file for the function and setting it to log_file
 
 
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+def sales_reports(log_file): # this is the same as making a function
+    for line in log_file:  # this loops trough the log file that is set the the function
+        line = line.rstrip() #This breaks down the lines in the file that is set in the function
+        day = line[0:3] #this looks for the first 3 charaters in the line from line 6 and sets them to the variable of day
+        if day == "Tue": # This looks to see if the the day that is indidated in line 7 is tusday
+            print(line) #if Line 8 is true then it will print the Line indicated in line 6
 
 
-sales_reports(log_file)
+sales_reports(log_file) #this calls the function for log_file that was made in line 1
+
